8fe51aa20f8944d96cdbf7746867865cb119aa6e
JBercich <joshbercich@gmail.com>
2023-02-04 13:00:17 +1100

    Rainbow quadrilaterals

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -23,7 +23,8 @@ int main() {
   TTF_Init();
 
 
-
+int Min = 0;
+int Max = 255;
 
   SDL_Window* window = SDL_CreateWindow("potato",
                                       SDL_WINDOWPOS_CENTERED,
@@ -31,13 +32,21 @@ int main() {
                                       WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
 
   
-  
+  SDL_Rect* rect;
   SDL_Renderer* rend = SDL_CreateRenderer(window, -1, 0);
   SDL_Event e;
   bool quit = false;
+
+
   bool FULL_SCREEN = FULL_SCREEN_DEFAULT;
   while (!quit){
       while (SDL_PollEvent(&e)){
+          SDL_PumpEvents();
+
+          // update keyboard state
+          Uint8* keysArray = const_cast <Uint8*> (SDL_GetKeyboardState(NULL));
+
+
           if (e.type == SDL_QUIT){
               quit = true;
           }
@@ -57,9 +66,47 @@ int main() {
             }
           }
         }
-        //   if (e.type == SDL_MOUSEBUTTONDOWN){
-        //       quit = true;
-        //   }
+
+        if (
+          e.type == SDL_MOUSEBUTTONDOWN && 
+          keysArray[SDL_SCANCODE_LSHIFT]
+        ) {
+            SDL_RenderClear(rend);
+
+            SDL_Rect rect;
+            rect.x = e.motion.x;
+            rect.y = e.motion.y;
+            rect.w = std::rand() % (Max + 1 - Min) + Min;
+            rect.h = std::rand() % (Max + 1 - Min) + Min;
+
+            SDL_SetRenderDrawColor(rend, std::rand() % (Max + 1 - Min) + Min, std::rand() % (Max + 1 - Min) + Min, std::rand() % (Max + 1 - Min) + Min, 255);
+            SDL_RenderFillRect(rend, &rect);
+
+            SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
+
+            SDL_RenderPresent(rend);
+          }
+
+          if (
+            e.type == SDL_MOUSEBUTTONDOWN && 
+            !keysArray[SDL_SCANCODE_LSHIFT]
+          ) {
+            
+            SDL_RenderClear(rend);
+
+            SDL_Rect rect;
+            rect.x = e.motion.x;
+            rect.y = e.motion.y;
+            rect.w = 200;
+            rect.h = 200;
+
+            SDL_SetRenderDrawColor(rend, 255, 1, 255, 255);
+            SDL_RenderFillRect(rend, &rect);
+
+            SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
+
+            SDL_RenderPresent(rend);
+          }
       }
   }
 
