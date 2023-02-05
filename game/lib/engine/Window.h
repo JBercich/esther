b1eebb02434bd28bf3336284ee0ceaf2b932c52c
JBercich <joshbercich@gmail.com>
2023-02-05 15:39:48 +1100

    Updated cmake and produce build metadata file

---
+++ game/lib/engine/Window.h
@@ -0,0 +1,25 @@
+/**
+ * @file Window.h
+ * @author Josh Bercich (joshbercich@gmail.com)
+ * @brief 
+ * @date 2023-02-05
+ * 
+ * Copyright (c) 2023 Josh Bercich (joshbercich@gmail.com)
+ * 
+ */
+#ifndef WINDOW_H
+#define WINDOW_H
+
+
+
+#define WINDOW_WIDTH 256
+#define WINDOW_HEIGHT
+#define HEIGHT 256
+#define FULL_SCREEN_DEFAULT false
+#define BACKGROUND_R 0
+#define BACKGROUND_G 0
+#define BACKGROUND_B 0
+#define BACKGROUND_A 255
+
+
+#endif
--- game/src/main.cpp
+++ game/src/main.cpp
@@ -9,108 +9,124 @@
 #include <string>
 #include <iostream>
 
-
 using namespace std;
 
 bool lshift = false;
 
-int main() {
+
+
+
+        int main()
+{
+
+
+
+
+
+
+
+  
   // report version
-  cout  << " Version " << VERSION_MAJOR << "."
-        << VERSION_MINOR <<  "\n";
+  cout << " Version " << VERSION_MAJOR << "."
+       << VERSION_MINOR << "\n";
   SDL_Init(SDL_INIT_VIDEO);
   IMG_Init(IMG_INIT_PNG);
   TTF_Init();
 
+  SDL_Window *window = SDL_CreateWindow("potato",
+                                        SDL_WINDOWPOS_CENTERED,
+                                        SDL_WINDOWPOS_CENTERED,
+                                        WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
 
-
-
-  SDL_Window* window = SDL_CreateWindow("potato",
-                                      SDL_WINDOWPOS_CENTERED,
-                                      SDL_WINDOWPOS_CENTERED,
-                                      WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
-
-
-
-  SDL_Renderer* rend = SDL_CreateRenderer(window, -1, 0);
+  SDL_Renderer *rend = SDL_CreateRenderer(window, -1, 0);
   SDL_Event e;
   bool quit = false;
   bool FULL_SCREEN = FULL_SCREEN_DEFAULT;
-  while (!quit){
-      while (SDL_PollEvent(&e)){
-        if (!lshift) {
-            if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_LSHIFT) {
-                lshift = true;
-            }
+  while (!quit)
+  {
+    while (SDL_PollEvent(&e))
+    {
+      if (!lshift)
+      {
+        if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_LSHIFT)
+        {
+          lshift = true;
         }
-        if (lshift) {
-            if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_LSHIFT) {
-                lshift = false;
-            }
+      }
+      if (lshift)
+      {
+        if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_LSHIFT)
+        {
+          lshift = false;
         }
-         if (e.type == SDL_QUIT){
-            quit = true;
+      }
+      if (e.type == SDL_QUIT)
+      {
+        quit = true;
+      }
+      if (e.type == SDL_KEYDOWN)
+      {
+        if (e.key.keysym.sym == SDLK_ESCAPE)
+        {
+          quit = true;
         }
-          if (e.type == SDL_KEYDOWN){
-            if (e.key.keysym.sym == SDLK_ESCAPE) {
-                quit = true;
-            }
-        }
-        
-        if (e.type == SDL_KEYDOWN){
-            if (e.key.keysym.sym == SDLK_f) {
-              if(!FULL_SCREEN) {
-                SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
-                FULL_SCREEN = !FULL_SCREEN;
-            } else {
-                SDL_SetWindowFullscreen(window, 0);
-                FULL_SCREEN = !FULL_SCREEN;
-            }
+      }
+
+      if (e.type == SDL_KEYDOWN)
+      {
+        if (e.key.keysym.sym == SDLK_f)
+        {
+          if (!FULL_SCREEN)
+          {
+            SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
+            FULL_SCREEN = !FULL_SCREEN;
+          }
+          else
+          {
+            SDL_SetWindowFullscreen(window, 0);
+            FULL_SCREEN = !FULL_SCREEN;
           }
         }
-          if (e.type == SDL_MOUSEBUTTONDOWN && lshift){
-            SDL_RenderClear(rend);
-            SDL_Rect rect;
-            rect.x = e.motion.x;
-            rect.y = e.motion.y;
-            rect.w = 32;
-            rect.h = 32;
-            SDL_SetRenderDrawColor(rend, 255,0,255,255);
-            SDL_RenderFillRect(rend, &rect);
-            SDL_SetRenderDrawColor(rend, 
-                        BACKGROUND_R,
-                        BACKGROUND_G,
-                        BACKGROUND_B,
-                        BACKGROUND_A);
-            SDL_RenderPresent(rend);
-            
-        }
+      }
+      if (e.type == SDL_MOUSEBUTTONDOWN && lshift)
+      {
+        SDL_RenderClear(rend);
+        SDL_Rect rect;
+        rect.x = e.motion.x;
+        rect.y = e.motion.y;
+        rect.w = 32;
+        rect.h = 32;
+        SDL_SetRenderDrawColor(rend, 255, 0, 255, 255);
+        SDL_RenderFillRect(rend, &rect);
+        SDL_SetRenderDrawColor(rend,
+                               BACKGROUND_R,
+                               BACKGROUND_G,
+                               BACKGROUND_B,
+                               BACKGROUND_A);
+        SDL_RenderPresent(rend);
+      }
 
-        //
+      //
 
+      // if (lshift && e.type == SDL_MOUSEBUTTONDOWN){
+      //     SDL_RenderClear(rend);
 
+      //     SDL_Circle circ;
 
-            
-        // if (lshift && e.type == SDL_MOUSEBUTTONDOWN){
-        //     SDL_RenderClear(rend);
-            
-        //     SDL_Circle circ;
+      //     circ.x = e.motion.x;
+      //     circ.y = e.motion.y;
+      //     circ.r = 16;
+      //     SDL_SetRenderDrawColor(rend, 255,0,255,255);
+      //     SDL_RenderFillRect(rend, &circ);
+      //     SDL_SetRenderDrawColor(rend,
+      //                 BACKGROUND_R,
+      //                 BACKGROUND_G,
+      //                 BACKGROUND_B,
+      //                 BACKGROUND_A);
+      //     SDL_RenderPresent(rend);
 
-
-        //     circ.x = e.motion.x;
-        //     circ.y = e.motion.y;
-        //     circ.r = 16;
-        //     SDL_SetRenderDrawColor(rend, 255,0,255,255);
-        //     SDL_RenderFillRect(rend, &circ);
-        //     SDL_SetRenderDrawColor(rend, 
-        //                 BACKGROUND_R,
-        //                 BACKGROUND_G,
-        //                 BACKGROUND_B,
-        //                 BACKGROUND_A);
-        //     SDL_RenderPresent(rend);
-            
-        // }
-          //
+      // }
+      //
     }
   }
 
@@ -120,5 +136,3 @@ int main() {
   SDL_Quit();
   return 0;
 }
-
-
