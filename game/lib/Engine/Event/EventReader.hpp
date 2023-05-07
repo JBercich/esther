a1c38c673c82960e76d5e33abbcf20506528dad7
JBercich <joshbercich@gmail.com>
2023-05-07 22:39:03 +0900

    bump

---
+++ game/lib/Engine/Event/EventReader.hpp
@@ -0,0 +1,9 @@
+#ifndef EVENT_READER_HPP
+#define EVENT_READER_HPP
+
+class EventReader {
+public:
+
+};
+
+#endif
---
+++ game/lib/Engine/Event/EventState.hpp
@@ -0,0 +1,8 @@
+#ifndef EVENT_STATE_HPP
+#define EVENT_STATE_HPP
+
+
+
+
+
+#endif
