76e881d3e4e59036d05400163bf3ad8c58a80270
JBercich <joshbercich@gmail.com>
2023-06-12 17:39:27 +0900

    Added basic LoggerManager documentation

--- include/Engine/Core/Managers/LoggerManager.hpp
+++ include/Engine/Core/Managers/LoggerManager.hpp
@@ -1,3 +1,13 @@
+/**
+ * @brief           Logging management controls the use of `spdlog` to setup the loggers
+ *                  and sinks with respective formatters and levels. Further control of
+ *                  log levels is performed in the top-level `Logger.hpp` with macros.
+ * 
+ * @file            LoggerManager.hpp
+ * @date            2023-06-12
+ * @author          Josh Bercich (joshbercich@gmail.com)
+ * @copyright       Copyright (c) 2023 Josh Bercich (joshbercich@gmail.com)
+ */
 
 #ifndef LOGGER_MANAGER_H
 #define LOGGER_MANAGER_H
@@ -6,12 +16,27 @@
 
 namespace Engine::Managers
 {
+    /**
+     * LoggerManager instances are responsible for the initialisation and shutdown of
+     * an `spdlog` environment with specific logger instances defined for the app
+     * engine and game abstractions.
+     */
     class LoggerManager 
     {
         public:
             LoggerManager() = default;
             ~LoggerManager() = default;
+
+            /**
+             * Initialise the LoggerManager instance by creating the logging sinks and
+             * separate loggers for the engine and game environments. Logger output
+             * patterns and (flush) levels are set respectively.
+             */
             void initialise();
+
+            /**
+             * Loggers from `spdlog` are closed and deallocated.
+             */
             void shutdown();
     };
 }
